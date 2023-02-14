028bac7744230425012eae6e10e64cb45346a674
Meet-Chaudhari <miteshchaudhari931@gmail.com>
2023-02-15 00:04:02 +0530

    Added files to git

---
+++ Basic Programs/P3_Palindrome_String.java
@@ -0,0 +1,22 @@
+public class P3_Palindrome_String {
+    public static void main(String[] args)
+    {
+        String str = "Radar", reverseStr = "";
+
+        int strLength = str.length();
+
+        for(int i = strLength - 1; i>=0; i--) 
+        {
+            reverseStr = reverseStr + str.charAt(i);
+        }
+
+        if(str.toLowerCase().equals(reverseStr.toLowerCase()))
+        {
+            System.out.println(str + " is Palindrome.");
+        }
+        else
+        {
+            System.out.println(str + " is not Palindrome.");
+        }
+    }
+}
